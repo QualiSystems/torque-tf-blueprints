87e775ed59286e4c53267172e4b42e8343456a40
Costya <costya.y@qualisystems.com>
2025-07-22 14:25:05 -0500

    fix: streamline variable exports and improve output formatting in get_openshift_vm_state.sh

--- scripts/get_openshift_vm_state.sh
+++ scripts/get_openshift_vm_state.sh
@@ -1,27 +1,22 @@
 echo "Getting outputs from OpenShift VM"
 export VM_NAME=$1
 export NAMESPACE=$2
-echo "VM Name: $VM_NAME"
-echo "Namespace: $NAMESPACE"
 echo $(kubectl wait vm/$VM_NAME -n $NAMESPACE --for=condition=Ready --timeout=400s)
-vm_json=$(kubectl get vm $VM_NAME -n $NAMESPACE -o json)
+export vm_json=$(kubectl get vm $VM_NAME -n $NAMESPACE -o json)
 export vm_name="$(echo "$vm_json" | jq -r '.metadata.name')"
-export storage="$vm_json" | jq -r '.spec.dataVolumeTemplates[]? | "- \(.metadata.name): size=\(.spec.persistentVolumeClaim.resources.requests.storage)"'
-export ip=$(kubectl get vmi $VM_NAME -n $NAMESPACE -o json   | jq -r ' .status.interfaces[]?.ipAddress // "N/A"'   | sed '/^N\/A$/d')
-secret_name=$(echo "$vm_json" \
-  | jq -r '.spec.template.spec.domain.devices.cloudInitNoCloud.userDataSecretRef.name // ""')
+export storage="$(echo "$vm_json" | jq -r '.spec.dataVolumeTemplates[]? | "\(.metadata.name) size=\(.spec.storage.resources.requests.storage)"')"
+echo "storage=$storage"
+echo ip=$(kubectl get vmi $VM_NAME -n $NAMESPACE -o json   | jq -r ' .status.interfaces[]?.ipAddress // "N/A"'   | sed '/^N\/A$/d')
+export secret_name=$(echo "$vm_json" \
+  | jq -r '.spec.template.spec.volumes[] | select(.name =="cloudinitdisk" ) | .cloudInitNoCloud.userData')
+# echo "secret_name=$secret_name"
 if [[ -n "$secret_name" ]]; then
-  echo "Credentials (from secret ${secret_name}):"
-  secret_json=$(kubectl get secret ${secret_name} -n $NAMESPACE -o json)
-  username=$(echo "$secret_json" \
-    | jq -r '.data.username' \
-    | base64 --decode)
-  password=$(echo "$secret_json" \
-    | jq -r '.data.password' \
-    | base64 --decode)
-  export user=$username
-  export password=$password
+  # echo "Credentials (from secret ${secret_name}):"
+  export user=$(echo "$vm_json" | jq -r '.spec.template.spec.volumes[] | select(.name == "cloudinitdisk") | .cloudInitNoCloud.userData' | grep '^user' | awk -F': ' '{print $2}')
+  export password=$(echo "$vm_json" | jq -r '.spec.template.spec.volumes[] | select(.name == "cloudinitdisk") | .cloudInitNoCloud.userData' | grep '^password:' | awk -F': ' '{print $2}')
 else
   export user=""
   export password=""
 fi
+echo "user=$user"
+echo "password=$password" 
