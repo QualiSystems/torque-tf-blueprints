49f6a21ebe343bf543493c1c10163e82b9dfbfd3
Constantine Yevdyukhin <costya.y@qualisystems.com>
2025-07-22 11:21:37 -0500

    reformat script a bit

--- scripts/get_openshift_vm_state.sh
+++ scripts/get_openshift_vm_state.sh
@@ -1,45 +1,22 @@
-echo "Getting External IP from service"
-sleep 60s
-set -euo pipefail
-
+echo "Getting outputs from OpenShift VM ${1} in namespace ${2}"
 VM_NAME="$1"
 NAMESPACE="$2"
-
-
 echo $(kubectl wait vmi/"${VM_NAME}" -n "${NAMESPACE}" --for=condition=Ready --timeout=400s)
-
-# 1. Fetch VM JSON
 vm_json=$(kubectl get vm "${VM_NAME}" -n "${NAMESPACE}" -o json)
-
-# 2. VM Name (just echoes back)
 export vm_name="$(echo "$vm_json" | jq -r '.metadata.name')"
-
-# 3. Attached Storage (via DataVolumeTemplates)
-export storage="$vm_json" | jq -r '
-  .spec.dataVolumeTemplates[]? |
-  "- \(.metadata.name): size=\(.spec.persistentVolumeClaim.resources.requests.storage)"
-'
-
-# 4. IP Addresses (from the corresponding VMI)
-export ip=$(kubectl get vmi "${VM_NAME}" -n "${NAMESPACE}" -o json   | jq -r '
-      .status.interfaces[]?.ipAddress // "N/A"
-    '   | sed '/^N\/A$/d')
-
-# 5. Credentials (if using CloudInit secret)
+export storage="$vm_json" | jq -r '.spec.dataVolumeTemplates[]? | "- \(.metadata.name): size=\(.spec.persistentVolumeClaim.resources.requests.storage)"'
+export ip=$(kubectl get vmi "${VM_NAME}" -n "${NAMESPACE}" -o json   | jq -r ' .status.interfaces[]?.ipAddress // "N/A"'   | sed '/^N\/A$/d')
 secret_name=$(echo "$vm_json" \
   | jq -r '.spec.template.spec.domain.devices.cloudInitNoCloud.userDataSecretRef.name // ""')
-
 if [[ -n "$secret_name" ]]; then
   echo "Credentials (from secret ${secret_name}):"
   secret_json=$(kubectl get secret "${secret_name}" -n "${NAMESPACE}" -o json)
-
   username=$(echo "$secret_json" \
     | jq -r '.data.username' \
     | base64 --decode)
   password=$(echo "$secret_json" \
     | jq -r '.data.password' \
     | base64 --decode)
-
   export user=$username
   export password=$password
 else
