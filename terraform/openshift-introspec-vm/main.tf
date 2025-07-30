39c2151b8da3e2c5535720bfde358a1c41eabda4
Costya <costya.y@qualisystems.com>
2025-07-30 12:35:51 -0500

    fix: update inputs in power-off workflow to use attributes for vm_name and namespace; correct vm_name input in power-on workflow; add vm_name to introspection_data in main.tf

--- terraform/openshift-introspec-vm/main.tf
+++ terraform/openshift-introspec-vm/main.tf
@@ -10,6 +10,7 @@ resource "torque_introspection_resource" "scratch_org_details" {
   display_name = var.vm_name
   image        = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/OpenShift-LogoType.svg/1200px-OpenShift-LogoType.svg.png"
   introspection_data = {
+    "VM Name": var.vm_name
     "Namespace": var.namespace
     "IP Address": var.ip
     "User": var.user
