cc05181252bb38ddd316a6449dd0a69c73856622
Costya <costya.y@qualisystems.com>
2025-07-31 15:00:18 -0500

    fix: export IP variable for OpenShift VM state retrieval

--- scripts/get_openshift_vm_state.sh
+++ scripts/get_openshift_vm_state.sh
@@ -1,5 +1,5 @@
 echo "Getting outputs from OpenShift VM"
-sleep 30
+# sleep 30
 export VM_NAME=$1
 export NAMESPACE=$2
 echo $(kubectl wait vm/$VM_NAME -n $NAMESPACE --for=condition=Ready --timeout=600s)
@@ -7,7 +7,8 @@ export vm_json=$(kubectl get vm $VM_NAME -n $NAMESPACE -o json)
 export vm_name="$(echo "$vm_json" | jq -r '.metadata.name')"
 export storage="$(echo "$vm_json" | jq -r '.spec.dataVolumeTemplates[]? | "\(.metadata.name) size=\(.spec.storage.resources.requests.storage)"')"
 echo "storage=$storage"
-echo ip=$(kubectl get vmi $VM_NAME -n $NAMESPACE -o json   | jq -r ' .status.interfaces[]?.ipAddress // "N/A"'   | sed '/^N\/A$/d')
+export ip=$(kubectl get vmi $VM_NAME -n $NAMESPACE -o json   | jq -r ' .status.interfaces[]?.ipAddress // "N/A"'   | sed '/^N\/A$/d')
+echo "ip=$ip"
 export secret_name=$(echo "$vm_json" \
   | jq -r '.spec.template.spec.volumes[] | select(.name =="cloudinitdisk" ) | .cloudInitNoCloud.userData')
 # echo "secret_name=$secret_name"
