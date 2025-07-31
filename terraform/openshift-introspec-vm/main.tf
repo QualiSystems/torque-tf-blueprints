f9b55c3b5f22d6b8dfa11d1df76f97e6ff37d467
DBS <96681520+davidbquali@users.noreply.github.com>
2025-07-31 14:31:40 -0500

    Update main.tf

--- terraform/openshift-introspec-vm/main.tf
+++ terraform/openshift-introspec-vm/main.tf
@@ -8,7 +8,7 @@ terraform {
 
 resource "torque_introspection_resource" "scratch_org_details" {
   display_name = var.vm_name
-  image        = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/OpenShift-LogoType.svg/1200px-OpenShift-LogoType.svg.png"
+  image        = "https://raw.githubusercontent.com/QualiTorque/Torque-Samples/refs/heads/main/icons/openshift-logo.svg"
   introspection_data = {
     "VM Name": var.vm_name
     "Namespace": var.namespace
